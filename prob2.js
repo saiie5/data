4f8e9bb8c9b27fd09d509b819a409f2cc260db09
saiie5 <56484744+saiie5@users.noreply.github.com>
2020-10-26 09:00:36 +0530

    problem1 solved

---
+++ prob2.js
@@ -0,0 +1,5 @@
+// Write a JavaScript program to display the current day and time in the following format.
+// Today is : Tuesday.
+
+console.log("to day is "+Date())
+
---
+++ problem1.js
@@ -0,0 +1,15 @@
+// Find pair with given sum in the array
+let twoSum = (array, sum) => {
+    let hashMap = {},
+      results = []
+
+        for (let i = 0; i < array.length; i++){
+            if (hashMap[array[i]]){
+                results.push([hashMap[array[i]], array[i]])
+            }else{
+                hashMap[sum - array[i]] = array[i];
+            }
+          }
+          return results;
+    }
+console.log(twoSum([10,20,10,40,50,60,70,30],50));
